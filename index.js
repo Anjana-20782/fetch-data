fff2ade027a18a18f4c6deec7d6f50f4434d0675
Anjana-20782 <anjanasivadasan0305@gmail.com>
2025-11-09 17:01:17 +0530

    new contents added

--- index.js
+++ index.js
@@ -5,5 +5,11 @@ fetch("https://jsonplaceholder.typicode.com/posts").then(response=>response.json
 console.log(data);
 let str=``
 
-
+data.forEach(ele => {
+    str+=`
+    <li><a href="./add.html${ele.id}">${ele.title}</a></li>
+    `
+})
+document.getElementById("list").innerHTML=str
+})
 })
