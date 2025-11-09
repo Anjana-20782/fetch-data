6df167d0854b5c9a5c637da12f089082dcf97d8d
Anjana-20782 <anjanasivadasan0305@gmail.com>
2025-11-09 17:01:05 +0530

    new contents added

---
+++ index.js
@@ -0,0 +1,9 @@
+
+document.getElementById("btn").addEventListener("click",()=>{
+fetch("https://jsonplaceholder.typicode.com/posts").then(response=>response.json())
+.then((data)=>{
+console.log(data);
+let str=``
+
+
+})
